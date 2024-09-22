477be70778cd02d283e6edd9fc2265261c3834e0
solarsungai <solarsungai@gmail.com>
2024-09-22 21:57:10 +0300

    feat: task solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.lastGuess = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.lastGuess = null;
     }
 
     guess() {
-
+        this.lastGuess = Math.round((this.min + this.max) / 2);
+        return this.lastGuess;
     }
 
     lower() {
-
+        this.max = this.lastGuess;
     }
 
     greater() {
-
+        this.min = this.lastGuess;
     }
 }
 
